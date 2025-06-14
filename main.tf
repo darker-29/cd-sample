422ad709dda8276fc478ac5bd44748af97aea815
daiki.ito <daiki.ito@daikiitonoMacBook-Pro.local>
2025-06-14 16:02:20 +0900

    add terraform config and resource

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "ditto-sandbox1"
+    key    = "terraform.tfstate"
+    region = "ap-northeast-1"
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
+# サンプルS3バケット
+resource "aws_s3_bucket" "sandbox1" {
+  bucket = "ditto-sandbox1-test"
+}
